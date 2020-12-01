4bb269b391cdd3bf2d2d457d40fcc7a5b099a21b
Brian Starke <brian.starke@gmail.com>
2020-11-30 23:53:43 -0500

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+  import { Router } from "@roxi/routify";
+  import { routes } from "../.routify/routes";
+</script>
+
+<style>
+  :global(body) {
+    --primary: #332717;
+    --secondary: #ff370f;
+    --accent: #6bacbf;
+    --background: #9dbcbc;
+
+    --body-font: "Montserrat", sans-serif;
+    --special-font: "Roboto Slab", serif;
+    transition: 0.6s;
+  }
+
+  :global(body.one) {
+    --primary: #ff0d51;
+    --secondary: #a88c8c;
+    --accent: #6bacbf;
+    --background: #909d9e;
+
+    --body-font: "Montserrat", sans-serif;
+    --special-font: "Roboto Slab", serif;
+  }
+  :global(body.two) {
+    --primary: #ff0d51;
+    --secondary: #a88c8c;
+    --accent: #6bacbf;
+    --background: #909d9e;
+
+    --body-font: "Montserrat", sans-serif;
+    --special-font: "Roboto Slab", serif;
+  }
+</style>
+
+<Router {routes} />
---
+++ src/pages/one/index.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+</script>
+
+<h1>one</h1>
+<p>here's a sample link right <a href="https://www.google.com">here</a></p>
+
+<p><button>click me</button></p>
